a394fb63aca8bf2faa4aaf991a6e357fbb51970a
joechesney <33613119+joechesney@users.noreply.github.com>
2018-02-01 12:04:24 -0600

    login now works

--- app/controllers/AuthCtrl.js
+++ app/controllers/AuthCtrl.js
@@ -3,14 +3,23 @@
 angular.module("PinterestApp").controller("AuthCtrl", function($scope, AuthFactory){
   $scope.auth = {};
 
-  $scope.registerUser = function(userInfo) {
-    AuthFactory.registerWithEmail(userInfo)
+  $scope.registerUser = function() {
+    AuthFactory.registerWithEmail($scope.auth)
     .then(function(userInfoFromFB) {
       console.log(userInfoFromFB);
       // log user in here, by passing userInfoFromFB into login method
     });
   };
 
+  $scope.logMeIn = function(){
+    AuthFactory.login($scope.auth)
+    .then(function(userInfoAfterLogin){
+        console.log('didLogin',userInfoAfterLogin);
+        $scope.login = {};
+        $scope.register = {};
+        // $location.url("/songs/list");
+      });
+    };
 
 
 });
