42f741c86fae437e56f172f5aafc644a4b4f197b
joechesney <33613119+joechesney@users.noreply.github.com>
2018-02-01 11:37:57 -0600

    got SOME of the auth

--- app/app.js
+++ app/app.js
@@ -1,6 +1,25 @@
 "use strict";
 
 angular.module("PinterestApp", ["ngRoute"])
-.constant("FBUrl", "pinteresting-53274.firebaseapp.com");
+.constant("FBUrl", "pinteresting-53274.firebaseapp.com")
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/song-list.html',
+      controller: 'SongListCtrl'
+    })
+    .when('/songs/new', {
+      templateUrl: 'partials/song-form.html',
+      controller: 'SongFormCtrl'
+    })
+    .when('/auth', {
+      templateUrl: 'partials/auth.html',
+      controller: 'AuthCtrl'
+    })
+    .otherwise('/auth');
+}])
+.run(function(FBCreds) {
+  firebase.initializeApp(FBCreds);
+});
 
 // routes and configs go here, chained onto the module definition
--- app/controllers/AuthCtrl.js
+++ app/controllers/AuthCtrl.js
@@ -1,7 +1,15 @@
 "use strict";
 
-angular.module("PinterestApp").controller("AuthCtrl", function($scope){
+angular.module("PinterestApp").controller("AuthCtrl", function($scope, AuthFactory){
+  $scope.auth = {};
 
+  $scope.registerUser = function(userInfo) {
+    AuthFactory.registerWithEmail(userInfo)
+    .then(function(userInfoFromFB) {
+      console.log(userInfoFromFB);
+      // log user in here, by passing userInfoFromFB into login method
+    });
+  };
 
 
 
