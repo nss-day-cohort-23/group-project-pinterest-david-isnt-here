db7675aa1929650cdb9424e6d0bdffd4c401119d
dhazlettjr <d.hazlett.jr@gmail.com>
2018-02-01 16:01:00 -0600

    created working link from partial to partial

--- app/controllers/ViewAllBoardsCtrl.js
+++ app/controllers/ViewAllBoardsCtrl.js
@@ -1,8 +1,14 @@
 "use strict";
 
-angular.module("PinterestApp").controller("ViewAllBoardsCtrl", function($scope){
+angular.module("PinterestApp").controller("ViewAllBoardsCtrl", function($scope, BoardFactory){
 
+    BoardFactory.getBoardData()
+        .then((boardData) => {
+            let keys = Object.keys(boardData.data);
+            keys.forEach(key => boardData.data[key].id = key);
+            $scope.boards = boardData.data;
+            console.log("is this the data?", boardData);
+            //  console.log("object", Object.entries(boardData.data));
 
-
-
+        });
 });
--- app/controllers/ViewBoardCtrl.js
+++ app/controllers/ViewBoardCtrl.js
@@ -1,8 +1,20 @@
 "use strict";
 
-angular.module("PinterestApp").controller("ViewBoardCtrl", function($scope){
+angular.module("PinterestApp").controller("ViewBoardCtrl", function($scope, BoardFactory, $routeParams, PinFactory){
+
+// BoardFactory.getBoardData()
+// .then ((boardData) => {
+//     console.log("is this the data?", boardData);
+//     // $scope.boardData = boardData.data;
+//     $scope.boardArray = Object.entries(boardData.data);
+
+//     return PinFactory.getPinData();
+// })
+// .then((pinData) => {
+//     console.log("pin data?", pinData);
+//     $scope.pinArray = Object.entries(pinData.data);
+    
+// });
 
 
-
-
-});
+ });
