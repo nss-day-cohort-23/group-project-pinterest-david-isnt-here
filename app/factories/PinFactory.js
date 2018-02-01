3a184b3f7c1e372b531c1b383064c44a6109e952
dhazlettjr <d.hazlett.jr@gmail.com>
2018-02-01 13:27:38 -0600

    created pin http calls

--- app/factories/PinFactory.js
+++ app/factories/PinFactory.js
@@ -1,8 +1,57 @@
 "use strict";
 
-angular.module("PinterestApp").factory("PinFactory", function(){
+angular.module("PinterestApp").factory("PinFactory", function ($q, $http, FBCreds) {
 
+    let getPinData = () => {
+        return $q(function (resolve, reject) {
+            $http.get(`${FBCreds}pins.json`)
+                .then(
+                (data) => {
+                    resolve(data);
+                })
+                .catch((err) => {
+                    reject(err);
+                })
+        });
+    }
 
+    let addPin = (pin) => {
+        return $q(function (resolve, reject) {
+            $http.post(`${FBCreds}pins.json`, JSON.stringify(pin))
+                .then(
+                (data) => {
+                    resolve(data);
+                })
+                .catch((err) => {
+                    reject(err);
+                })
+        });
+    }
 
+    let deletePin = () => {
+        return $q(function (resolve, reject) {
+            $http.delete(`${FBCreds}pins.json`)
+                .then(
+                (data) => {
+                    resolve(data);
+                })
+                .catch((err) => {
+                    reject(err);
+                })
+        });
+    }
 
+    let editPin = () => {
+        return $q(function (resolve, reject) {
+            $http.put(`${FBCreds}pins.json`)
+                .then(
+                (data) => {
+                    resolve(data);
+                })
+                .catch((err) => {
+                    reject(err);
+                })
+        });
+    }
+    return { getPinData, addPin, deletePin, editPin };
 });
