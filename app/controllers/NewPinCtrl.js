11eefca27af0724abfaa7ef7ce85a6d6f24ff7e1
David Lars Ketch <davidlarsketch@gmail.com>
2018-02-02 11:19:26 -0600

    Includes redirect for savePintToFB in NewPinCrl

--- app/controllers/NewPinCtrl.js
+++ app/controllers/NewPinCtrl.js
@@ -1,21 +1,18 @@
 "use strict";
 
 angular.module("PinterestApp")
-.controller("NewPinCtrl", function($scope, $routeParams, PinFactory){
+.controller("NewPinCtrl", function($scope, $routeParams, $location, PinFactory) {
 
   $scope.newPin = {};
   $scope.newPin.title = "";
   $scope.newPin.url = "";
 
-  // adds the current users uid and the boardid (from routeParams) 
+  // adds the current users uid and the boardid (from routeParams)
   // onto the newPin object before posting
-  $scope.savePinToFB = () =>{
+  $scope.savePinToFB = () => {
     $scope.newPin.uid = firebase.auth().currentUser.uid;
     $scope.newPin.boardid = $routeParams.bid;
     PinFactory.addPin($scope.newPin)
-    .then((pinResponse)=>{
-        // console.log('pinResponsepin',pinResponse);
-        // TODO: make page redirect back to the board
-    });
+    .then(pinResponse => $location.url(`/board/${$routeParams.bid}`));
   };
 });
