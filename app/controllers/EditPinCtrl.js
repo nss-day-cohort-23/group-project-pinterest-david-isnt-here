03de37b9d101eb90b4576f5d080de947faff0b46
David Lars Ketch <davidlarsketch@gmail.com>
2018-02-02 11:20:20 -0600

    Adds delete() to EditPinCtrl which deletes selected board from DB; completes TODO, using .url() to redirect after successful savePinToFB()

--- app/controllers/EditPinCtrl.js
+++ app/controllers/EditPinCtrl.js
@@ -3,20 +3,21 @@
 angular.module("PinterestApp")
 .controller("EditPinCtrl", function($scope, PinFactory, $location, $window){
 
-  
+
   PinFactory.getOnePin().then(oldPin => {
     $scope.newPin = oldPin;
     $scope.title = `Editing "${oldPin.title}"`;
   });
-  
+
   $scope.savePinToFB = () => {
     PinFactory.editPin($scope.newPin)
-    .then((data) => {
-      $window.location.href =`#!/board/${data.boardid}/pin/${data.id}`;
-      // TODO: make this locatoin.url????
-    });
+    .then(data => $location.url(`#!/board/${data.boardid}/pin/${data.id}`));
   };
 
-  
+  $scope.delete = pinid => {
+    let deletedPin = $scope.newPin;
+    PinFactory.deletePin(pinid)
+    .then(() => $location.url(`/board/${deletedPin.boardid}`));
+  };
 
 });
