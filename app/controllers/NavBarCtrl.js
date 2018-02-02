16c06014c9eca832a05e55c27e759dc8a7831d89
dhazlettjr <d.hazlett.jr@gmail.com>
2018-02-02 13:31:00 -0600

    formatted navbar and added logout button

--- app/controllers/NavBarCtrl.js
+++ app/controllers/NavBarCtrl.js
@@ -2,19 +2,24 @@
 
 angular.module('PinterestApp')
 
-.controller('NavBarCtrl', ['$scope', '$location', function
-($scope, $location) {
+  .controller('NavBarCtrl', ['$scope', '$location', function ($scope, $location, AuthFactory) {
 
-$scope.isActive = function(path){
+    $scope.isActive = function (path) {
 
-   var currentPath = $location.path().split('/')[1];
+      var currentPath = $location.path().split('/')[1];
 
-   if (currentPath.indexOf('?') !== -1) {
+      if (currentPath.indexOf('?') !== -1) {
 
-     currentPath = currentPath.split('?')[0];
-   }
-   return currentPath === path.split('/')[1];
+        currentPath = currentPath.split('?')[0];
+      }
+      return currentPath === path.split('/')[1];
 
-};
+    };
+    $scope.logout = () => {
+      AuthFactory.logout()
+        .then(() => {
+          $location.path("/login");
+        });
+    };
 
-}]);
+  }]);
