28d21866bbf132bb2700051d1e10b8ac5e5c404b
David Lars Ketch <davidlarsketch@gmail.com>
2018-02-02 11:18:27 -0600

    Adds delete() to ViewAllBoardsCtrl which deletes selected board from DB; ES6-ifies the rest

--- app/controllers/ViewAllBoardsCtrl.js
+++ app/controllers/ViewAllBoardsCtrl.js
@@ -1,10 +1,14 @@
 "use strict";
 
 angular.module("PinterestApp")
-.controller("ViewAllBoardsCtrl", function($scope, BoardFactory){
+.controller("ViewAllBoardsCtrl", function($scope, BoardFactory) {
 
-    BoardFactory.getAllBoards()
-        .then((boardData) => {
-            $scope.boards = boardData;
-        });
+  BoardFactory.getAllBoards()
+  .then(boardData => $scope.boards = boardData);
+
+  $scope.delete = boardid => {
+    BoardFactory.deleteBoard(boardid)
+    .then(() => BoardFactory.getAllBoards())
+    .then(boardData => $scope.boards = boardData);
+  };
 });
