5d092025245474b3d52bc5db7bb6cb78c0889ac3
joechesney <33613119+joechesney@users.noreply.github.com>
2018-02-05 11:29:04 -0600

    removed some stuff

--- app/controllers/NavBarCtrl.js
+++ app/controllers/NavBarCtrl.js
@@ -1,33 +1,8 @@
 'use strict';
 
 angular.module('PinterestApp')
-.controller('NavBarCtrl', function (FBCreds, $q, $http, $scope, $window, $location, AuthFactory, $routeParams) {
-    // $scope.boardid = $routeParams.bid;
-    // console.log('scopeboardid',$scope.boardid);
-    //  let getBoardName = (boardid) =>{
-    //   return $q((resolve, reject)=>{
-    //     $http.get(`${FBCreds.DBurl}/boards.json`)
-    //     .then(({data}) => {
-    //       let keys = Object.keys(data);
-    //       keys.forEach(key => data[key].boardid = key);
-    //       let newData = Object.values(data);
-    //       console.log('newData',newData);
-    //       resolve(newData);
-    //     })
-    //     .catch((err) => {
-    //       reject(err);
-    //     });
-    //   });
-    // };
-    // getBoardName()
-    // .then(boardArray =>{
-    //   console.log('boardArray',boardArray);
-    //   boardArray.forEach(board=>{
-    //     if(board.boardid === $scope.boardid){
-    //       console.log('THE RIGHT ONE',board);
-    //     }
-    //   });
-    // });
+.controller('NavBarCtrl', function ( $scope, $window, $location, AuthFactory) {
+    
     $scope.isActive = function (path) {
       let currentPath = $location.path().split('/')[1];
       if (currentPath.indexOf('?') !== -1)
