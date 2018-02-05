5b4ec1fe1a963d534f4386d31e96955a276101b2
joechesney <33613119+joechesney@users.noreply.github.com>
2018-02-05 13:17:43 -0600

    removed no pins, my dude

--- app/controllers/ViewBoardCtrl.js
+++ app/controllers/ViewBoardCtrl.js
@@ -4,7 +4,6 @@ angular.module("PinterestApp")
 .controller("ViewBoardCtrl", function($scope, $routeParams, $location, PinFactory, AuthFactory){
   // TODO: find less janky solution
   $scope.boardid = $routeParams.bid;
-  $scope.boardTitle = "No Pins, my dude";
   AuthFactory.getUser()
   .then(user => PinFactory.getAllPins($scope.boardid))
   .then(pinData => $scope.pins = pinData)
